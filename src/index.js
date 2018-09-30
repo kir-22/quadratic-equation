e3373ba9e1fc0762cc41ae5c3318ec1cd7255641
kir-22 <kirill89kireew@gmail.com>
2018-09-30 21:12:09 +0300

    quadratic-equation: use  ES6.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 module.exports = function solveEquation(equation) {
   // your implementation
     equation=equation.replace(/\s/g, '');
-    var array=[];
-    var indexA=equation.search(/\^/);
-    var a=parseInt(equation.slice(0,indexA));
-    var indexB=equation.search(/\*x[+-]/);
-    var b=parseInt(equation.slice(indexA+2,indexB));
-    var c=parseInt(equation.slice(indexB+2));
+    const array=[];
+    let indexA=equation.search(/\^/),
+        a=parseInt(equation.slice(0,indexA)),
+        indexB=equation.search(/\*x[+-]/),
+        b=parseInt(equation.slice(indexA+2,indexB)),
+        c=parseInt(equation.slice(indexB+2));
 
-    var discremenant=(b*b)-(4*a*c);
+    let discremenant=(b*b)-(4*a*c);
     if(discremenant>0){
-        var x1=Math.round((-b+Math.sqrt(discremenant))/(2*a));
-        var x2=Math.round((-b-Math.sqrt(discremenant))/(2*a));
+        let x1=Math.round((-b+Math.sqrt(discremenant))/(2*a)),
+            x2=Math.round((-b-Math.sqrt(discremenant))/(2*a));
 
         array.push(x1,x2);
-        return array.sort(function (a,b) {
+        return array.sort((a,b)=> {
             return a-b;
         });
 
     }else if(discremenant==0){
-        var x=(-b)/(2*a);
+        let x=(-b)/(2*a);
         array.push(x);
         return array;
     }
